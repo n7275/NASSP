3020d460bc0da247e5eff6e31a7e1409388c5712
Matthew W Hume <mtthwhm@gmail.com>
2021-11-10 01:35:25 -0500

    Fuel cell nitrogen pressurization system

--- Orbitersdk/samples/ProjectApollo/src_csm/saturn.h
+++ Orbitersdk/samples/ProjectApollo/src_csm/saturn.h
@@ -3632,6 +3632,7 @@ protected:
 	Cooling *FuelCellCooling[3];
 	h_Tank *FuelCellO2Manifold[3];
 	h_Tank *FuelCellH2Manifold[3];
+	h_Tank *FuelCellN2Blanket[3];
 
 	// O2 tanks.
 	h_Tank *O2Tanks[2];
