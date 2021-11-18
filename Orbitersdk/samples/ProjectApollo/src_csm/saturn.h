6d2a2d9620e881eef2d935609a05d85135aadbb4
Matthew W Hume <mtthwhm@gmail.com>
2021-11-18 00:52:23 -0500

    stabilized flow and temp; issue with FC2 loop

--- Orbitersdk/samples/ProjectApollo/src_csm/saturn.h
+++ Orbitersdk/samples/ProjectApollo/src_csm/saturn.h
@@ -3685,6 +3685,7 @@ protected:
 	//EPS Cooling System
 	double *FuelCellCoolantInletTemp[3];
 	double *FuelCellCoolantOutletTemp[3];
+	double *FuelCellConsenserTemp[3];
 	Pump *EPScoolantPump[3];
 	h_Pipe *CoolantReturn[3];
 	h_Pipe *CoolantPipeRad_5_6[3];
