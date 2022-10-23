1ad46d92a06362c6de3903f8392d2d625e1078d1
Matthew W Hume <mtthwhm@gmail.com>
2022-10-22 23:15:33 -0400

    update critical pressure/temperature based on internal calcs

--- Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Hsystems.h
+++ Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Hsystems.h
@@ -45,8 +45,8 @@ const double SPECIFICC_GAS	[MAX_SUB]=	{0.658,		10.183,		1.4108,		0.743,		0.6553,
 const double SPECIFICC_LIQ	[MAX_SUB]=	{1.1519,	9.668,		4.184,		1.7848,		0.858,		3.691041,		1.0724,			1.5525,			5.193		};		//J/g-K .. assume constant
 const double L_DENSITY		[MAX_SUB]=	{1141.0,	70.0,		1000.0,		807.0,		1014.0,		1038.5,			899.0,			1450.0,			0.164		};		//g/L @ 103kPa ..assume constant wrt. temp
 const double BULK_MOD		[MAX_SUB]=	{32e6,		24e6,		2.18e6,		32e6,		32e6,		2.55e6,			1.47397e6,		1.362e6,		10e6		};		//Pa .. assume constant and converted from m^3 to L
-const double CRITICAL_P		[MAX_SUB]=  {350115.0,	89631.0,	1523741.0,	234421.0,	508833.0,	3097574.75,		11692906.154,	10132500.0,		226968.0224 };		//Pa.. critical pressure
-const double CRITICAL_T		[MAX_SUB]=  {154.7,		33.2,		647.3,		126.2,		304.4,		256.9525,		607.15,			431.15,			5.19		};		//K.. critical temperature
+const double CRITICAL_P		[MAX_SUB]=  {4926063.722233855,	1303085.962148109,	19910602.43884709,	3300253.307024634,	7800213.020695794,	3097566.149152389,		14500299.44342058,	9998268.775903003,		228416.3259285132 };		//Pa.. critical pressure
+const double CRITICAL_T		[MAX_SUB]=  {154.7,		33.2,		647.3,		126.2,		304.4,		645,			607.15,			431.15,			5.19		};		//K.. critical temperature
 const double ANTIONE_A		[MAX_SUB]=	{9.3199656,	6.59723,	12.490095,	9.0020008,	12.0892,	8.32957,		13.7222,		14.47645,		4.41952 };			//Antione Equation A constant gives results in bar, must be converter to Pa	[1]
 const double ANTIONE_B		[MAX_SUB]=	{838.91339,	133.793,	4658.1375,	694.78356,	2353.762,	3158.1575,		5309.7973,		4256.07694,		18.65037 };			//Antione Equation B constant gives results in bar, must be converter to Pa	[2]
 const double ACENTRIC		[MAX_SUB]=  {0.022,		-0.216,		0.345,		0.040,		0.288,		0.416,			0.316,			0.0141345,			-0.390};		//[3] Acentric factor
@@ -96,6 +96,7 @@ class h_volume
 	void operator+=(h_volume);		//add two volumes together
 	void operator+=(h_substance);	//or simply add some sub. to the volume
 	h_volume Break(double vol, int* mask, double maxMass = 0);		//break 'vol' liters from the volume ..into another volume
+	double RemoveLiquidMass(double grams); //useful for propellant tanks and pumping out of a sump under aceleration
 	void GetMaxSub();				//re-computes number of substances present in the volume
 	double GetMass();				//total mass inside the volume
 	double GetQ();
