f9169f7276e0a7a2aa555921524a537e75a4d8be
Matthew W Hume <mtthwhm@gmail.com>
2023-07-02 20:36:04 -0400

    allow setting flowmax from config file

--- Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Hsystems.cpp
+++ Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Hsystems.cpp
@@ -853,7 +853,7 @@ void h_Tank::BoilAllAndSetTemp(double _t) {
 
 //------------------------------- PIPE CLASS ------------------------------------
 
-h_Pipe::h_Pipe(char *i_name, h_Valve *i_IN, h_Valve *i_OUT, int i_type, double max, double min, int is_two) { 
+h_Pipe::h_Pipe(char *i_name, h_Valve *i_IN, h_Valve *i_OUT, int i_type, double max, double min, int is_two, double maxFlow) { 
 
 	strcpy(name, i_name);
 	max_stage = 99;
@@ -865,7 +865,7 @@ h_Pipe::h_Pipe(char *i_name, h_Valve *i_IN, h_Valve *i_OUT, int i_type, double m
 	out = i_OUT;
 	open = 0;
 	flow = 0;
-	flowMax = 0;
+	flowMax = maxFlow;
 }
 
 void h_Pipe::BroadcastDemision(ship_object * gonner) {
